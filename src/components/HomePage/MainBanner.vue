41dde2f087b1c6c97706f255407d7e5518f2a935
AntixxTechHub <info@antixxtechhub.com>
2023-05-05 17:25:31 +0530

    PRICE CARD UPDATE

--- src/components/HomePage/MainBanner.vue
+++ src/components/HomePage/MainBanner.vue
@@ -3,9 +3,8 @@
         <div class="screenshots-area pb-100">
             <div class="container">
                 <div class="section-title">
-                    <h2>Experience Unbeatable Sales Efficiency with DealDocx NOW!</h2>
-                    <p>Are you still using spreadsheets? Toss it! and start using DealDox now generate AI-Enabled quotes
-                        with just few clicks</p>
+                    <h2>Experience Unbeatable Sales <br>Efficiency with DealDocx NOW!</h2>
+                    <p>Are you still using spreadsheets? Toss it! and start using DealDox now <br>generate AI-Enabled quotes with just few clicks</p>
                     <router-link to="/sign-in" class="default-btn">Boost Your Sales Now</router-link>
                     <div class="section-title-button">
                     <p>It’s completely FREE to start</p>
