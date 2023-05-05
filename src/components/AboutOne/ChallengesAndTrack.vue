d03ed9b4c17e2b253985ce08d14acc8d17fa4655
AntixxTechHub <info@antixxtechhub.com>
2023-05-05 15:40:10 +0530

    new code

--- src/components/AboutOne/ChallengesAndTrack.vue
+++ src/components/AboutOne/ChallengesAndTrack.vue
@@ -24,7 +24,7 @@
 
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
-                    <div class="about-content">
+                    <div class="about-content-right">
                         <h3>Professional Services</h3>
                         <p>The powerful AI-driven and data-driven features of DealDox transform the estimation, pricing, and
                             quotation of services. It aids in maximizing gross margins and improving win probability by
@@ -38,7 +38,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/professional-services.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/DocumentAnalytics.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -48,7 +48,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/managed-services.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/Documentwithphone.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -62,7 +62,7 @@
 
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
-                    <div class="about-content">
+                    <div class="about-content-right">
                         <h3>Subscription Services</h3>
                         <p>DealDox facilitates work within any pricing model — fixed, tiered, or pay-per-usage. Setup fees, discount management, renewal pricing, and mid-contract updates are all possibilities. Add-ons and upgrades can also be attached to current contract terms and conditions</p>
                     </div>
@@ -71,7 +71,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/subscription-services.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/Productprice.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -81,7 +81,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/everything-as-a-service.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/Database.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -95,7 +95,7 @@
 
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
-                    <div class="about-content">
+                    <div class="about-content-right">
                         <h3>Reduction in document management</h3>
                         <p>Time spent on document management has moderately decreased for our customers. DealDox automates the presales process, allowing the sales team to concentrate on selling while spending less time on other tasks. Additionally, users can create SOWs with no risk and 100% accuracy.</p>
                     </div>
@@ -104,7 +104,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/Reduction-in-document.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/secondary-shapes.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -114,7 +114,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/enhanced-margins.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/Womanearningthroughpassiveincome.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -128,7 +128,7 @@
 
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
-                    <div class="about-content">
+                    <div class="about-content-right">
                         <h3>Swift Implementation</h3>
                         <p>DealDox revenue platform was designed for any type of industry, and no need for additional coding. As a result, you can easily incorporate your technology stack without any complicated procedures that typically necessitate months' worth of resources. Its implementation is easy and swift - 10x quicker than with other service quotes.</p>
                     </div>
@@ -137,7 +137,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/swift-implementation.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/ProjectManagementSoftware.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
@@ -147,7 +147,7 @@
                 <div class="col-lg-6 col-md-12">
                     <div class="features-item with-border">
                         <div class="about-img">
-                            <img src="../../assets/images/cpq/streamlined-quoting-process.png" data-aos="fade-up" alt="about">
+                            <img src="../../assets/images/cpq/ProjectInitiation.png" data-aos="fade-up" alt="about">
                         </div>
                     </div>
                 </div>
