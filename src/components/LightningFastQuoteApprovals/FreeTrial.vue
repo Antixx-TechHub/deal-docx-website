ca0f9b683078abcf830c795a092292750eee313e
AntixxTechHub <info@antixxtechhub.com>
2023-05-09 11:19:19 +0530

    css issue fixed

--- src/components/LightningFastQuoteApprovals/FreeTrial.vue
+++ src/components/LightningFastQuoteApprovals/FreeTrial.vue
@@ -3,9 +3,8 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12">
-                    <div class="features-item-free-quotes with-border">
+                    <div class="features-item-free-quotes with-border bg-grey">
                         <h3>Automating quote generation boosts creativity and streamlines workflow, Fact-based quotes maintain accuracy while keeping a unique voice.</h3>
-                        <br>
                         <router-link to="/sign-in" class="default-btn">Get your free quotes now!</router-link>
                     </div>
                 </div>
